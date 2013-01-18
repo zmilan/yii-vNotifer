7e7194ad535762b3c048071c6e0cd2beb3b81bbe
Gabor Pihaj <pgee@vdx.hu>
2013-01-18 22:10:37 +0100

    Nice UI for NotificationWidget

--- yii-vNotifier/notificationWidget/NotificationWidget.php
+++ yii-vNotifier/notificationWidget/NotificationWidget.php
@@ -18,6 +18,7 @@ class NotificationWidget extends CWidget {
 		if(Yii::app()->user->canReceiveNotification()) {
 			$commonAssetUrl = Yii::app()->assetManager->publish(Yii::getPathOfAlias('ext.yii-vNotifier.assets'),false,-1,true);
 			$widgetAssetUrl = Yii::app()->assetManager->publish(Yii::getPathOfAlias('ext.yii-vNotifier.notificationWidget.assets'),false,-1,true);
+			Yii::app()->clientScript->registerCssFile($widgetAssetUrl.'/notification-widget.css');
 			Yii::app()->clientScript->registerCoreScript('jquery');
 			Yii::app()->clientScript->registerScriptFile($widgetAssetUrl.'/knockout-2.2.0.js');
 			Yii::app()->clientScript->registerScriptFile($this->getNotifier()->socketioUrl.'/socket.io/socket.io.js');
--- yii-vNotifier/notificationWidget/views/notificationWidget.php
+++ yii-vNotifier/notificationWidget/views/notificationWidget.php
@@ -1,9 +1,11 @@
 <h1>Notifications</h1>
 <div id="notification-area">
-	<ul data-bind="foreach: notifications">
-		<li data-bind="text: message"></li>
+	<ul data-bind="template : {foreach: notifications, beforeRemove: beforeRemove, afterAdd : afterAdd }">
+		<li class="gradient" data-bind="text: message"></li>
 	</ul>
 </div>
+<script type="text/html">
+</script>
 <?php
 	/* @var $this NotificationWidget */
 	$config = CJSON::encode(array(
@@ -19,7 +21,13 @@
 		var notificationArea = document.getElementById('notification-area'),
 			// KnockoutJS View Model
 			viewModel = {
-				notifications : ko.observableArray()
+				notifications : ko.observableArray(),
+				beforeRemove : function(el) {
+					$(el).fadeOut(400);
+				},
+				afterAdd : function(el) {
+					$(el).hide().fadeIn(400);
+				}
 			};
 
 		ko.applyBindings(viewModel,notificationArea);
